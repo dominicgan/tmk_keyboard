85a48dc9f82cc27dc99e9a3516b5d9546a0167ba
Dominic Gan <dominicgangx@gmail.com>
2017-07-06 01:38:45 +0800

    updated keymap

--- converter/xt_usb/keymap_common.h
+++ converter/xt_usb/keymap_common.h
@@ -61,12 +61,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     { KC_##K20, KC_##K21, KC_##K22, KC_##K23, KC_##K24, KC_##K25, KC_##K26, KC_##K27 }, \
     { KC_##K28, KC_##K29, KC_##K2A, KC_##K2B, KC_##K2C, KC_##K2D, KC_##K2E, KC_##K2F }, \
     { KC_##K30, KC_##K31, KC_##K32, KC_##K33, KC_##K34, KC_##K35, KC_##K36, KC_##K37 }, \
-    { KC_##K38, KC_##K39, KC_##K3A, KC_##K3B, KC_##K3C, KC_##K3D, KC_##K3E, KC_##K3F }, \
+    { KC_##K38, KC_##K39, KC_##K3A, KC_##K3B, KC_##K3C, KC_##K3D, KC_##K3E, KC_NO    }, \
     { KC_##K40, KC_##K41, KC_##K42, KC_##K43, KC_##K44, KC_##K45, KC_##K46, KC_##K47 }, \
     { KC_##K48, KC_##K49, KC_##K4A, KC_##K4B, KC_##K4C, KC_##K4D, KC_##K4E, KC_##K4F }, \
     { KC_##K50, KC_##K51, KC_##K52, KC_##K53, KC_##K54, KC_##K55, KC_##K56, KC_##K57 }, \
     { KC_##K58, KC_##K59, KC_##K5A, KC_##K5B, KC_##K5C, KC_##K5D, KC_##K5E, KC_##K5F }, \
-    { KC_##K60, KC_##K61, KC_##K62, KC_##K63, KC_##K64, KC_##K65, KC_##K66, KC_##K67 }, \
+    { KC_##K60, KC_NO,    KC_NO,    KC_##K63, KC_##K64, KC_##K65, KC_##K66, KC_##K67 }, \
     { KC_##K68, KC_##K69, KC_##K6A, KC_##K6B, KC_##K6C, KC_##K6D, KC_##K6E, KC_##K6F }, \
     { KC_##K70, KC_##K71, KC_##K72, KC_##K73, KC_##K74, KC_##K75, KC_##K76, KC_##K77 }, \
     { KC_##K78, KC_##K79, KC_##K7A, KC_##K7B, KC_##K7C, KC_##K7D, KC_##K7E, KC_##K7F }, \
--- converter/xt_usb/keymap_ibm.c
+++ converter/xt_usb/keymap_ibm.c
@@ -28,17 +28,9 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     SLCK,INT4,  TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,      ENT,  DEL, END, PGDN,  P7,  P8,  P9,  PPLS,
     PAUS,INT5,  LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,                      UP,         P4,  P5,  P6, 
     APP, INT6,  LSFT,LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,BSLS,     RSFT,  LEFT,INT2,RGHT,  P1,  P2,  P3,  PENT,
-    RGUI,LGUI,  LALT,     LGUI,          SPC,SPC, SPC,                      LGUI,     RALT,       DOWN,       P0,  PDOT,
+    RGUI,LGUI,  LALT,     LGUI,          SPC,SPC, SPC,                      LGUI,     RALT,       DOWN,       P0,  PDOT
     ),
 };
-				  K74,K75,K76,K77,K78,K79,K7A,K7B,K7C,K7D,K7E,K7F,                                         \
-                  K68,K69,K6A,K6B,K6C,K6D,K6E,K6F,K70,K71,K72,K73,                                         \
-                                                                                                           \
-    K48,K49,  K3D,K24,K25,K26,K27,K28,K29,K2A,K2B,K2C,K2D,K2E,K2F,K30,K3E,  K52,K53,K54,  K64,K65,K66,K67, \
-    K46,K47,  K3C,K18,K19,K1A,K1B,K1C,K1D,K1E,K1F,K20,K21,K22,K23,    K3B,  K4F,K50,K51,  K5D,K5E,K5F,K63, \
-    K44,K45,  K3A,K0C,K0D,K0E,K0F,K10,K11,K12,K13,K14,K15,K16,K17,              K4E,      K5A,K5B,K5C,     \
-    K42,K43,  K38,K01,K02,K03,K04,K05,K06,K07,K08,K09,K0A,K0B,        K39,  K4B,K4C,K4D,  K57,K58,K59,K60, \
-    K40,K41,  K31,    K32,        K33,K34,K35,                K36,    K37,      K4A,      K55,K56          \
 
 const action_t PROGMEM fn_actions[] = {
 };
