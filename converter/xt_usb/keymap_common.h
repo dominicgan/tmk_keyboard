52d1a5faead8bc5f6988cda5f95310a23cf79eff
Dominic Gan <dominicgangx@gmail.com>
2017-07-06 01:19:47 +0800

    new keymap based on hid_listen

--- converter/xt_usb/keymap_common.h
+++ converter/xt_usb/keymap_common.h
@@ -28,40 +28,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "keymap.h"
 
 
-/* All keys */
-#define KEYMAP_ALL( \
-    K00,    K02,K03,K04,K05,K06,K07,K08,K09,K0A,K0B,K0C,K0D, K0E,K0F,K0G,                   \
-    K10,K11,K12,K13,K14,K15,K16,K17,K18,K19,K1A,K1B,K1C,K1D, K1E,K1F,K1G,  K1H,K1I,K1J,K1K, \
-    K20,K21,K22,K23,K24,K25,K26,K27,K28,K29,K2A,K2B,K2C,K2D, K2E,K2F,K2G,  K2H,K2I,K2J,     \
-    K30,K31,K32,K33,K34,K35,K36,K37,K38,K39,K3A,K3B,    K3D,               K3H,K3I,K3J,K3K, \
-    K40,K41,K42,K43,K44,K45,K46,K47,K48,K49,K4A,        K4D,     K4F,      K4H,K4I,K4J,     \
-    K50,K51,K52,        K55,                K5A,K5B,K5C,K5D, K5E,K5F,K5G,  K5H,    K5J,K5K, \
-                                                                                            \
-    K60, K61, K62,           /* System Power, Sleep, Wake */                                \
-    K70, K71, K72,           /* Mute, Volume Up, Volume Down */                             \
-    K80, K81, K82, K83, K84, /* Next, Previous, Stop, Pause, Media Select */                \
-    K90, K91, K92,           /* Mail, Calculator, My Computer */                            \
-    KA0, KA1, KA2, KA3,      /* WWW Search, Home, Back, Forward */                          \
-    KB0, KB1, KB2            /* WWW Stop, Refresh, Favorites */                             \
-) { \
-    { KC_NO,    KC_##K00, KC_##K11, KC_##K12, KC_##K13, KC_##K14, KC_##K15, KC_##K16 }, \
-    { KC_##K17, KC_##K18, KC_##K19, KC_##K1A, KC_##K1B, KC_##K1C, KC_##K1D, KC_##K20 }, \
-    { KC_##K21, KC_##K22, KC_##K23, KC_##K24, KC_##K25, KC_##K26, KC_##K27, KC_##K28 }, \
-    { KC_##K29, KC_##K2A, KC_##K2B, KC_##K2C, KC_##K3D, KC_##K50, KC_##K31, KC_##K32 }, \
-    { KC_##K33, KC_##K34, KC_##K35, KC_##K36, KC_##K37, KC_##K38, KC_##K39, KC_##K3A }, \
-    { KC_##K3B, KC_##K10, KC_##K40, KC_##K2D, KC_##K41, KC_##K42, KC_##K43, KC_##K44 }, \
-    { KC_##K45, KC_##K46, KC_##K47, KC_##K48, KC_##K49, KC_##K4A, KC_##K4D, KC_##K1J }, \
-    { KC_##K52, KC_##K55, KC_##K30, KC_##K02, KC_##K03, KC_##K04, KC_##K05, KC_##K06 }, \
-    { KC_##K07, KC_##K08, KC_##K09, KC_##K0A, KC_##K0B, KC_##K1H, KC_##K0F, KC_##K2H }, \
-    { KC_##K2I, KC_##K2J, KC_##K1K, KC_##K3H, KC_##K3I, KC_##K3J, KC_##K3K, KC_##K4H }, \
-    { KC_##K4I, KC_##K4J, KC_##K5H, KC_##K5J, KC_##K5K, KC_##K5D, KC_##K1I, KC_##K0C }, \
-    { KC_##K0D, KC_##K5A, KC_##K80, KC_##K51, KC_##K5B, KC_##K5C, KC_##K60, KC_##K61 }, \
-    { KC_##K70, KC_##K91, KC_##K83, KC_##K62, KC_##K82, KC_##KA0, KC_##KB2, KC_##KB1 }, \
-    { KC_##KB0, KC_##KA3, KC_##KA2, KC_##K92, KC_##K90, KC_##K84, KC_##K81, KC_##K1F }, \
-    { KC_##K4F, KC_##K1G, KC_##K72, KC_##K5E, KC_##K71, KC_##K5G, KC_##KA1, KC_##K2F }, \
-    { KC_##K5F, KC_##K2G, KC_##K1E, KC_##K2E, KC_##K0E, KC_##K0G, KC_NO   , KC_NO    }  \
-}
-
 /* 
  * IBM Terminal keyboard 6110345(122keys)/1392595(102keys)
  * http://geekhack.org/showthread.php?10737-What-Can-I-Do-With-a-Terminal-Model-M
@@ -76,17 +42,19 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * 17|         |
  *   +---------+
  */
-#define KEYMAP_IBM( \
-				  K08,K10,K18,K20,K28,K30,K38,K40,K48,K50,K57,K5F,                                         \
-                  K07,K0F,K17,K1F,K27,K2F,K37,K3F,K47,K4F,K56,K5E,                                         \
+
+/* All keys */
+#define KEYMAP_ALL( \
+				  K74,K75,K76,K77,K78,K79,K7A,K7B,K7C,K7D,K7E,K7F,                                         \
+                  K68,K69,K6A,K6B,K6C,K6D,K6E,K6F,K70,K71,K72,K73,                                         \
                                                                                                            \
-    K05,K06,  K0E,K16,K1E,K26,K25,K2E,K36,K3D,K3E,K46,K45,K4E,K55,K5D,K66,  K67,K6E,K6F,  K76,K77,K7E,K84, \
-    K04,K0C,  K0D,K15,K19,K24,K2D,K2C,K35,K3C,K43,K44,K4D,K54,K5B,    K5C,  K64,K65,K6D,  K6C,K75,K7D,K7C, \
-    K03,K0B,  K14,K1C,K1B,K23,K2B,K34,K11,K3B,K42,K4B,K4C,K52,    K53,K5A,      K63,      K6B,K73,K74,K7B, \
-    K83,K0A,  K12,K13,K1A,K22,K21,K2A,K32,K31,K3A,K41,K49,K4A,    K51,K59,  K61,K62,K6A,  K69,K72,K7A,K79, \
-    K01,K09,  K33,    K1D,            K29,                    K39,    K58,      K60,      K68,K70,K71,K78  \
+    K48,K49,  K3D,K24,K25,K26,K27,K28,K29,K2A,K2B,K2C,K2D,K2E,K2F,K30,K3E,  K52,K53,K54,  K64,K65,K66,K67, \
+    K46,K47,  K3C,K18,K19,K1A,K1B,K1C,K1D,K1E,K1F,K20,K21,K22,K23,    K3B,  K4F,K50,K51,  K5D,K5E,K5F,K63, \
+    K44,K45,  K3A,K0C,K0D,K0E,K0F,K10,K11,K12,K13,K14,K15,K16,K17,              K4E,      K5A,K5B,K5C,     \
+    K42,K43,  K38,K01,K02,K03,K04,K05,K06,K07,K08,K09,K0A,K0B,        K39,  K4B,K4C,K4D,  K57,K58,K59,K60, \
+    K40,K41,  K31,    K32,        K33,K34,K35,                K36,    K37,      K4A,      K55,K56          \
 ) { \
-    { KC_NO,    KC_##K01, KC_NO,    KC_##K03, KC_##K04, KC_##K05, KC_##K06, KC_##K07 }, \
+    { KC_NO,    KC_##K01, KC_##K02, KC_##K03, KC_##K04, KC_##K05, KC_##K06, KC_##K07 }, \
     { KC_##K08, KC_##K09, KC_##K0A, KC_##K0B, KC_##K0C, KC_##K0D, KC_##K0E, KC_##K0F }, \
     { KC_##K10, KC_##K11, KC_##K12, KC_##K13, KC_##K14, KC_##K15, KC_##K16, KC_##K17 }, \
     { KC_##K18, KC_##K19, KC_##K1A, KC_##K1B, KC_##K1C, KC_##K1D, KC_##K1E, KC_##K1F }, \
@@ -101,10 +69,30 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     { KC_##K60, KC_##K61, KC_##K62, KC_##K63, KC_##K64, KC_##K65, KC_##K66, KC_##K67 }, \
     { KC_##K68, KC_##K69, KC_##K6A, KC_##K6B, KC_##K6C, KC_##K6D, KC_##K6E, KC_##K6F }, \
     { KC_##K70, KC_##K71, KC_##K72, KC_##K73, KC_##K74, KC_##K75, KC_##K76, KC_##K77 }, \
-    { KC_##K78, KC_##K79, KC_##K7A, KC_##K7B, KC_##K7C, KC_##K7D, KC_##K7E, KC_NO    }, \
-    { KC_NO,    KC_NO,    KC_NO,    KC_##K83, KC_##K84, KC_NO,    KC_NO,    KC_NO,   }, \
+    { KC_##K78, KC_##K79, KC_##K7A, KC_##K7B, KC_##K7C, KC_##K7D, KC_##K7E, KC_##K7F }, \
 }
-                  
+
+#define KEYMAP_IBM( \
+				  K74,K75,K76,K77,K78,K79,K7A,K7B,K7C,K7D,K7E,K7F,                                         \
+                  K68,K69,K6A,K6B,K6C,K6D,K6E,K6F,K70,K71,K72,K73,                                         \
+                                                                                                           \
+    K48,K49,  K3D,K24,K25,K26,K27,K28,K29,K2A,K2B,K2C,K2D,K2E,K2F,K30,K3E,  K52,K53,K54,  K64,K65,K66,K67, \
+    K46,K47,  K3C,K18,K19,K1A,K1B,K1C,K1D,K1E,K1F,K20,K21,K22,K23,    K3B,  K4F,K50,K51,  K5D,K5E,K5F,K63, \
+    K44,K45,  K3A,K0C,K0D,K0E,K0F,K10,K11,K12,K13,K14,K15,K16,K17,              K4E,      K5A,K5B,K5C,     \
+    K42,K43,  K38,K01,K02,K03,K04,K05,K06,K07,K08,K09,K0A,K0B,        K39,  K4B,K4C,K4D,  K57,K58,K59,K60, \
+    K40,K41,  K31,    K32,        K33,K34,K35,                K36,    K37,      K4A,      K55,K56          \
+) \
+KEYMAP_ALL( \
+				  K74,K75,K76,K77,K78,K79,K7A,K7B,K7C,K7D,K7E,K7F,                                         \
+                  K68,K69,K6A,K6B,K6C,K6D,K6E,K6F,K70,K71,K72,K73,                                         \
+                                                                                                           \
+    K48,K49,  K3D,K24,K25,K26,K27,K28,K29,K2A,K2B,K2C,K2D,K2E,K2F,K30,K3E,  K52,K53,K54,  K64,K65,K66,K67, \
+    K46,K47,  K3C,K18,K19,K1A,K1B,K1C,K1D,K1E,K1F,K20,K21,K22,K23,    K3B,  K4F,K50,K51,  K5D,K5E,K5F,K63, \
+    K44,K45,  K3A,K0C,K0D,K0E,K0F,K10,K11,K12,K13,K14,K15,K16,K17,              K4E,      K5A,K5B,K5C,     \
+    K42,K43,  K38,K01,K02,K03,K04,K05,K06,K07,K08,K09,K0A,K0B,        K39,  K4B,K4C,K4D,  K57,K58,K59,K60, \
+    K40,K41,  K31,    K32,        K33,K34,K35,                K36,    K37,      K4A,      K55,K56          \
+)
+
 /* US layout */
 #define KEYMAP( \
     K76,K05,K06,K04,K0C,K03,K0B,K83,K0A,K01,K09,K78,K07,     KFC,K7E,KFE,                   \
--- converter/xt_usb/keymap_ibm.c
+++ converter/xt_usb/keymap_ibm.c
@@ -24,13 +24,21 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                      F13, F14, F15, F16, F17, F18, F19, F20, F21, F22, F23, F24,
                      F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12,
 
-    PSCR,ESC,   ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, NO,  BSPC,  INS, HOME,PGUP,  NLCK,PSLS,PAST,PMNS,
-    SLCK,INT4,  TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,     NO,    DEL, END, PGDN,  P7,  P8,  P9,  PPLS,
-    PAUS,INT5,  LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     BSLS,ENT,        UP,         P4,  P5,  P6,  PCMM,
-    APP, INT6,  LSFT,LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,     NO,  RSFT,  LEFT,INT2,RGHT,  P1,  P2,  P3,  PENT,
-    RGUI,LGUI,  LCTL,     LALT,               SPC,                          LGUI,     GRV,        DOWN,       NO,  P0,  PDOT,NO
+    PSCR,ESC,   ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, GRV, BSPC,  INS, HOME,PGUP,  NLCK,PSLS,PAST,PMNS,
+    SLCK,INT4,  TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,      ENT,  DEL, END, PGDN,  P7,  P8,  P9,  PPLS,
+    PAUS,INT5,  LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,                      UP,         P4,  P5,  P6, 
+    APP, INT6,  LSFT,LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,BSLS,     RSFT,  LEFT,INT2,RGHT,  P1,  P2,  P3,  PENT,
+    RGUI,LGUI,  LALT,     LGUI,          SPC,SPC, SPC,                      LGUI,     RALT,       DOWN,       P0,  PDOT,
     ),
 };
+				  K74,K75,K76,K77,K78,K79,K7A,K7B,K7C,K7D,K7E,K7F,                                         \
+                  K68,K69,K6A,K6B,K6C,K6D,K6E,K6F,K70,K71,K72,K73,                                         \
+                                                                                                           \
+    K48,K49,  K3D,K24,K25,K26,K27,K28,K29,K2A,K2B,K2C,K2D,K2E,K2F,K30,K3E,  K52,K53,K54,  K64,K65,K66,K67, \
+    K46,K47,  K3C,K18,K19,K1A,K1B,K1C,K1D,K1E,K1F,K20,K21,K22,K23,    K3B,  K4F,K50,K51,  K5D,K5E,K5F,K63, \
+    K44,K45,  K3A,K0C,K0D,K0E,K0F,K10,K11,K12,K13,K14,K15,K16,K17,              K4E,      K5A,K5B,K5C,     \
+    K42,K43,  K38,K01,K02,K03,K04,K05,K06,K07,K08,K09,K0A,K0B,        K39,  K4B,K4C,K4D,  K57,K58,K59,K60, \
+    K40,K41,  K31,    K32,        K33,K34,K35,                K36,    K37,      K4A,      K55,K56          \
 
 const action_t PROGMEM fn_actions[] = {
 };
